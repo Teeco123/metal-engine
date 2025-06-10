fb49e4246637f0fc897d7e79aa8f2ddddad29488
Kacper Gajko <kacper.gajko1@icloud.com>
2025-06-10 22:40:21 +0200

    display window in render loop while drawing

--- Examples/BareDemo/main.swift
+++ Examples/BareDemo/main.swift
@@ -1,10 +1,16 @@
+import Foundation
 import MetalEngine
 import Utils
 
 @main
 struct BareDemo {
+    @MainActor
+    static var engine: MetalEngine?
+
     static func main() {
         Logger.info("Bare demo started")
-        MetalEngine.initialize()
+        engine = MetalEngine()
+        engine?.startRenderingLoop()
+        RunLoop.main.run()
     }
 }
--- Sources/MetalEngine/MetalEngine.swift
+++ Sources/MetalEngine/MetalEngine.swift
@@ -1,10 +1,51 @@
+import Foundation
+import Metal
+import QuartzCore
 import Renderer
 import Utils
 
 public class MetalEngine {
-    public static func initialize() {
+    var renderTimer: Timer?
+    var device: MTLDevice
+    var commandQueue: MTLCommandQueue
+    var layer: CAMetalLayer
+
+    public init() {
         Logger.info("MetalEngine starting up...")
-        let device = Device.commandQueue.device
-        let layer = MetalLayer.layer
+        device = Device.device
+        commandQueue = Device.commandQueue
+        layer = MetalLayer.layer
+    }
+
+    public func startRenderingLoop() {
+        renderTimer = Timer.scheduledTimer(
+            withTimeInterval: 1.0 / 60.0, repeats: true
+        ) { [weak self] _ in
+            self?.draw()
+        }
+    }
+
+    public func stopRenderingLoop() {
+        renderTimer?.invalidate()
+        renderTimer = nil
+    }
+
+    func draw() {
+        let drawable = MetalLayer.drawable
+
+        let passDescriptor = MTLRenderPassDescriptor()
+        passDescriptor.colorAttachments[0].texture = drawable.texture
+        passDescriptor.colorAttachments[0].loadAction = .clear
+        passDescriptor.colorAttachments[0].clearColor = MTLClearColorMake(0.1, 0.2, 0.3, 1.0)
+        passDescriptor.colorAttachments[0].storeAction = .store
+
+        guard let commandBuffer = self.commandQueue.makeCommandBuffer(),
+              let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: passDescriptor)
+        else { return }
+
+        renderEncoder.endEncoding()
+        commandBuffer.present(drawable)
+        commandBuffer.commit()
+        Logger.info("dupa")
     }
 }
